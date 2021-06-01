Fix softmax add dim

train.py
@@ -442,7 +442,7 @@ def train(args, labeled_trainloader, unlabeled_trainloader, test_loader,
             max_probs, targets_u = torch.max(pseudo_label, dim=-1)
             mask = max_probs.ge(args.threshold).float()
 
-            Lu = (F.l1_loss(torch.softmax(logits_u_s), F.one_hot(targets_u, 10),
+            Lu = (F.l1_loss(torch.softmax(logits_u_s, -1), F.one_hot(targets_u, 10),
                                   reduction='none') * mask).mean()
 
             loss = Lx - args.lambda_u * Lu
@@ -507,7 +507,7 @@ def train(args, labeled_trainloader, unlabeled_trainloader, test_loader,
                 max_probs, targets_u = torch.max(pseudo_label, dim=-1)
                 mask = max_probs.ge(args.threshold).float()
 
-                Lu = (F.l1_loss(torch.softmax(logits_u_s), F.one_hot(targets_u, 10),
+                Lu = (F.l1_loss(torch.softmax(logits_u_s, -1), F.one_hot(targets_u, 10),
                                   reduction='none') * mask).mean()
 
                 loss = Lu